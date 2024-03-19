de88c02f9c572f6fb7403b522a86d28c35f23513
Iraiore <yagoiraiore@gmail.com>
2024-03-19 00:06:43 -0400

    firs commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace blazor_application_calcular_imc.Models
+{
+    public class Pessoa
+    {
+        public string? Nome {  get; set; }
+        public double? Peso { get; set; }
+        public double? Altura { get; set; }
+        public double? ResultadoIMC {  get; set; }
+        public string? ClassificacaoIMC { get; set; }
+
+    }
+}
